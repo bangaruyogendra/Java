be443229375cb2c984b51cedb7878aca77313ab6
bangaruyogendra <111693322+bangaruyogendra@users.noreply.github.com>
2024-12-08 01:50:20 +0000

    Reverse number

--- Basics/Rcursion_Level1/Reverse_Number.java
+++ Basics/Rcursion_Level1/Reverse_Number.java
@@ -6,16 +6,26 @@ public class Reverse_Number {
     public static void main(String[] args) {
         //Reverse the Number
         Scanner sc =new Scanner(System.in);
-        rev(sc.nextInt());
-        System.out.println(sum);
+        int res =rev2(sc.nextInt());
+        System.out.println(res);
     }
-    static int sum=0;
-    static void rev(int n){
-        if (n==0){
-            return;
+    // static void rev(int n){
+    //     if (n==0){
+    //         return;
+    //     }
+    //     int rem = n%10;
+    //     sum = sum*10+rem;
+    //     rev(n/10);
+    // }
+    static int rev2(int n){
+       int digits = (int)(Math.log10(n))+1;
+       return helper(n,digits);
+    }
+    static int helper(int n,int digits){
+        if(n%10 ==n){
+            return n;
         }
         int rem = n%10;
-        sum = sum*10+rem;
-        rev(n/10);
+        return (int) (rem * Math.pow(10, digits-1)+helper(n/10, digits-1));
     }
 }
