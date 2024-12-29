5cec1745582094e898623a30621dc2f037779bf3
bangaruyogendra <111693322+bangaruyogendra@users.noreply.github.com>
2024-12-29 03:25:45 +0000

    Letters Combinations of numbers

--- Basics/Recursion_Strings/Letter_Combination_of_Letter.java
+++ Basics/Recursion_Strings/Letter_Combination_of_Letter.java
@@ -2,36 +2,23 @@ package Basics.Recursion_Strings;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Letter_Combination_of_Letter {
     //for example given string "23" then return {ad,ae,af,bd,be,bf,cd,ce,cf}
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        String str = sc.next();
-        ArrayList<String> li = new ArrayList<>();
-        for(int i=0;i<str.length();i++){
-           char ch = str.charAt(i);
-           int num = ch-'0';
-           switch (num) {
-            case 1:
-                break;
-           
-            case 2:
-                li.add("abc");
-                break;
-            case 3:
-                li.add("def");
-                break;
-           }
-        }
-        letter_combinations(li);
+        String name = "12";
+        letters(" ",name);
 
     }
-    static void letter_combinations(ArrayList<String> str){
-        ArrayList<String> ans = helper(str.get(0),str.get(1));
-        System.out.println(ans);
-    }
-    static ArrayList<String> helper(String up,String down){
-        if()
+    static void letters(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        int num = up.charAt(0)-'0';
+        
+        for(int i=(num-1)*3;i<num*3;i++){
+            char ch = (char)('a'+i);
+            letters(p+ch, up.substring(1));
+        }
     }
 }
