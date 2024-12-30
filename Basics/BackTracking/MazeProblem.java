8032dd35d35d15c8e32fe6e4ecd21cb7b7d9f8f5
bangaruyogendra <111693322+bangaruyogendra@users.noreply.github.com>
2024-12-30 03:30:16 +0000

    complete maze with Backtrack

--- Basics/BackTracking/MazeProblem.java
+++ Basics/BackTracking/MazeProblem.java
@@ -1,6 +1,7 @@
 package Basics.BackTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MazeProblem {
@@ -18,9 +19,12 @@ public class MazeProblem {
         //Maze(" ",n,n);
         //mazeDiagnal(" ",n,n);
         boolean[][] matrix  ={{true,true,true},
-        {true,false,true},
+        {true,true,true},
         {true,true,true}};
-        mazeRestrications(" ",matrix,n,n);
+        //mazeRestrications(" ",matrix,n,n);
+        //mazeBacktrack(" ",matrix,0,0);
+        int[][] path = new int[matrix.length][matrix[0].length];
+        mazeBacktrackpathPrint(" ",matrix,0,0,path,1);
         
     }
     // static void Maze(boolean[][] matrix,int r,int c,List<List<Integer>> path){
@@ -73,20 +77,76 @@ public class MazeProblem {
     //         mazeDiagnal(p+'C',r-1,c-1);
     //     }
     // }
-    static void mazeRestrications(String p,boolean[][] maze,int r,int c){
-        if(r == 1 && c ==1){
-            System.out.println(p);
-            return;
+    // static void mazeRestrications(String p,boolean[][] maze,int r,int c){
+    //     if(r == 1 && c ==1){
+    //         System.out.println(p);
+    //         return;
+    //     }
+    //     if(!maze[r-1][c-1]){
+    //         return;
+    //     }
+    //     if(r>1){
+    //         mazeRestrications(p+'D',maze,r-1,c);
+    //     }
+    //     if(c>1){
+    //         mazeRestrications(p+'R',maze,r,c-1);
+    //     }
+    // }
+    // static void mazeBacktrack(String p,boolean[][] maze,int r,int c){
+        
+    //     if(r == maze.length-1 && c ==maze.length-1 ){
+    //         System.out.println(p);
+    //         return;
+    //     }
+    //     if(!maze[r][c]){
+    //         return;
+    //     }
+    //     maze[r][c] =false;
+    //     if(r<maze.length-1){
+    //         mazeBacktrack(p+'D',maze,r+1,c);
+    //     }
+    //     if(c<maze.length-1){
+    //         mazeBacktrack(p+'R',maze,r,c+1);
+    //     }
+    //     if(r>1){
+    //         mazeBacktrack(p+'U', maze, r-1, c);
+    //     }
+    //     if(c>1){
+    //         mazeBacktrack(p+'L',maze,r,c-1);
+    //     }
+    //     maze[r][c]=true;
+    // }
+    
+    static void mazeBacktrackpathPrint(String p,boolean[][] maze,int r,int c,int[][] path,int step){
+        
+            if(r == maze.length-1 && c ==maze.length-1 ){
+                path[r][c]=step;
+                for(int[] arr:path){
+                    System.out.println(Arrays.toString(arr));
+                }
+                System.out.println(p);
+                System.out.println();
+                return;
+            }
+            if(!maze[r][c]){
+                return;
+            }
+            maze[r][c] =false;
+            path[r][c] =step;
+            if(r<maze.length-1){
+                mazeBacktrackpathPrint(p+'D', maze, r+1, c, path, step+1);
+            }
+            if(c<maze.length-1){
+                mazeBacktrackpathPrint(p+'R', maze, r, c+1, path, step+1);
+            }
+            if(r>1){
+                mazeBacktrackpathPrint(p+'U', maze, r-1, c,path,step+1);
+            }
+            if(c>1){
+                mazeBacktrackpathPrint(p+'L',maze,r,c-1,path,step+1);
+            }
+            maze[r][c]=true;
+            path[r][c]=0;
         }
-        if(!maze[r-1][c-1]){
-            return;
-        }
-        if(r>1){
-            mazeRestrications(p+'D',maze,r-1,c);
-        }
-        if(c>1){
-            mazeRestrications(p+'R',maze,r,c-1);
-        }
-    }
     
 }
